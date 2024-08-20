8bc94d2d7a3e6c0c602e214ea2d80aa1c9dccdef
MCheping8108 <liuyiming53@outlook.com>
2024-08-20 08:32:15 +0800

    新增功能：清除玩家身上所有效果

--- src/main/java/top/peacefuly/bedhelther/BedHelth/BedHelth.java
+++ src/main/java/top/peacefuly/bedhelther/BedHelth/BedHelth.java
@@ -4,12 +4,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.potion.PotionEffect;
 
 public class BedHelth implements Listener {
     @EventHandler
     void onEntryBed(PlayerBedEnterEvent e) {
+        Player player = e.getPlayer();
         if (e.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
             e.getPlayer().setHealth(e.getPlayer().getMaxHealth());
+//            清空玩家身上所有效果
+            for (PotionEffect effect : player.getActivePotionEffects()){
+                player.removePotionEffect(effect.getType());
+            }
         }
     }
 }
